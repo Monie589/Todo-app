b061e0dd6bd215f43a4b8026ddfdab3bd2806b14
Moniea <moniea589@gmail.com>
2022-06-09 19:00:37 +0630

    title change (2)

--- src/page/Task.js
+++ src/page/Task.js
@@ -16,7 +16,7 @@ function Task() {
   };
   return (
     <div className="container">
-      <h1 className="header">Shopping List</h1>
+      <h1 className="header">My Todo List</h1>
       <hr />
       <form className="form" onSubmit={addTask}>
         <input type="type" className="input" placeholder="Create new task" value={form.note} onChange={(e) => setForm({ ...form, note: e.target.value })} />
